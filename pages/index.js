0a04c5621d11954461a6bb4d1f7c8d20ceb51b2d
sagar <sagar_sys@live.com>
2020-10-21 09:38:19 +0400

    - add title

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
 import Head from 'next/head'
 import Jumbotron from 'react-bootstrap/Jumbotron'
-import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button'
 
 export default function Home() {
-  return (
-    <div >
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>Spin and Win game</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <Jumbotron className="pb-1">
-        <h1 className="header">Welcome To React-Bootstrap</h1>
-        <h2 className="header">Using Sass with custom theming</h2>
-      </Jumbotron>
+            <Jumbotron className="pb-1">
+                <h1 className="header">Welcome To React-Bootstrap</h1>
+                <h2 className="header">Using Sass with custom theming</h2>
+            </Jumbotron>
 
-      <Button>test</Button>
-    </div>
-  )
+            <Button>test</Button>
+        </div>
+    )
 }
