fb601859d5f0bc89dbb2279e2887c6228ffcd5a0
sagar <sagar_sys@live.com>
2020-10-21 11:33:30 +0400

    - rearrange import order

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import '../styles/globals.scss'
 import '../styles/theme.scss'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />
 }
 
 export default MyApp
