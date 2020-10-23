ba7e742696ad89cb21927e4aa9347ab893941955
sagar <sagar_sys@live.com>
2020-10-23 17:07:24 +0400

    - make the form work with react hook form

--- components/BettingForm.js
+++ components/BettingForm.js
@@ -6,12 +6,35 @@ import styles from '../styles/components/BettingForm.module.scss'
 import CustomRadio from './CustomRadio'
 import Button from 'react-bootstrap/Button'
 import TermsAndConditions from './TermsAndConditions'
+import { Controller, useForm } from 'react-hook-form'
+import { useState } from 'react'
+import { parseMultiplierFromOddsString } from '../lib/string'
+
+const defaultValues = {
+    odds: 'x2',
+    stake: 25,
+}
+const INITIAL_BALANCE = 1000
 
 export default function BettingForm() {
+    const { handleSubmit, control, watch } = useForm({
+        defaultValues,
+        mode: 'onBlur',
+    })
+    const [data, setData] = useState(null)
+    const [balance, setBalance] = useState(INITIAL_BALANCE)
+    const onSubmit = (data) => {
+        console.log('submit', data)
+        setData(data)
+    }
+    const calculatePossibleWin = () => {
+        return watch('stake') * parseMultiplierFromOddsString(watch('odds'))
+    }
+
     return (
         <Col md={5}>
             <Jumbotron>
-                <Form>
+                <Form onSubmit={handleSubmit(onSubmit)}>
                     <Row className="pb-4">
                         <Col xs={12}>
                             <h2 className={styles.formTitle}>
@@ -19,9 +42,17 @@ export default function BettingForm() {
                             </h2>
                             <hr />
                         </Col>
-                        <CustomRadio />
                     </Row>
-
+                    <Controller
+                        render={({ onChange }) => (
+                            <CustomRadio onChange={onChange} />
+                        )}
+                        name="odds"
+                        control={control}
+                        rules={{
+                            required: true,
+                        }}
+                    />
                     <Row className="pb-4">
                         <Col xs={12}>
                             <h2 className={styles.formTitle}>
@@ -29,27 +60,30 @@ export default function BettingForm() {
                             </h2>
                             <hr />
                         </Col>
-                        <Col xs={6} className={styles.stake}>
+                        <Col xs={12} className={styles.stake}>
                             <Form.Group controlId="stake">
                                 <Form.Label className={styles.formLabel}>
                                     Stake
                                 </Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Enter stake"
-                                    value={25}
-                                    size="lg"
+                                <Controller
+                                    as={
+                                        <Form.Control
+                                            type="number"
+                                            placeholder="Enter stake"
+                                            min={25}
+                                            max={500}
+                                            size="lg"
+                                        />
+                                    }
+                                    rules={{ required: true }}
+                                    name="stake"
+                                    control={control}
                                 />
-                                <Form.Text className="text-muted">
+
+                                <Form.Text className="text-info">
                                     Min 25 | Max 500.
                                 </Form.Text>
                             </Form.Group>
-                        </Col>
-                        <Col xs={6} className={styles.possibleWin}>
-                            <p className="text-uppercase mb-0">Possible Win</p>
-                            <p className="text-success">100</p>
-                            <p className="text-uppercase mb-0">Balance</p>
-                            <p className="text-success">1000</p>
                         </Col>
                     </Row>
 
@@ -67,6 +101,16 @@ export default function BettingForm() {
                             >
                                 Click to spin
                             </Button>
+                        </Col>
+                        <Col xs={6} className={styles.possibleWin}>
+                            <p className="text-uppercase mb-0">Possible Win</p>
+                            <p className="text-success">
+                                {calculatePossibleWin()}
+                            </p>
+                        </Col>
+                        <Col xs={6} className={styles.possibleWin}>
+                            <p className="text-uppercase mb-0">Balance</p>
+                            <p className="text-success">{balance}</p>
                         </Col>
                     </Row>
                 </Form>
