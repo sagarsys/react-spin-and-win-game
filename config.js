d6837496096a7a76070b0a6f550b03316e7cfe6c
sagar <sagar_sys@live.com>
2020-10-25 23:54:38 +0400

    - add db config

--- config.js
+++ config.js
@@ -2,3 +2,8 @@ export const IS_PROD = process.env.NODE_ENV === 'production'
 export const SERVER_BASE_URL = IS_PROD
     ? 'https://react-spin-and-win-game.vercel.app'
     : 'http://localhost:3000'
+
+// !! ONLY FOR DEMO PURPOSES => DB INFO SHOULD NOT BE COMMITTED OR EASILY ACCESSIBLE
+export const DB_NAME = 'pesawheel'
+export const DB_USER = 'pesawheel'
+export const DB_PASS = 'mongo1234'
