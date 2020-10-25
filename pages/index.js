2cd99c156ee2b0505e78ea914591756a0b8f8828
sagar <sagar_sys@live.com>
2020-10-25 13:10:18 +0400

    - wrap the index comp within the state provider

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
-import Head from 'next/head'
+import { StateProvider } from '../store/store'
 import Layout from '../components/Layout'
+import Head from 'next/head'
 import Game from '../components/Game'
 
 export default function Home() {
     return (
-        <Layout>
-            <Head>
-                <title>Spin and Win game</title>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <meta name="description" content="Spin and win game" />
-                <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-            </Head>
+        <StateProvider>
+            <Layout>
+                <Head>
+                    <title>Spin and Win game</title>
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1"
+                    />
+                    <meta name="description" content="Spin and win game" />
+                    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+                </Head>
 
-            <Game />
-        </Layout>
+                <Game />
+            </Layout>
+        </StateProvider>
     )
 }
