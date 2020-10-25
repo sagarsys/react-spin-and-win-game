e238d11f41bd06a72047ab23fbc387d553738802
sagar <sagar_sys@live.com>
2020-10-25 13:08:37 +0400

    - add context hook api store + useReducer

---
+++ store/actions.js
@@ -0,0 +1,2 @@
+export const INIT_GAME = 'Init game'
+export const SET_SEGMENTS_TO_SPIN = 'Set segments to spin'
---
+++ store/store.js
@@ -0,0 +1,30 @@
+import React, { createContext, useReducer } from 'react'
+import { INIT_GAME } from './actions'
+
+const initialState = {}
+const store = createContext(initialState)
+const { Provider } = store
+
+function reducer(state, action) {
+    const { type } = action
+    // console.log('reducer', state, action)
+    switch (type) {
+        case INIT_GAME:
+            const { payload } = action
+            // init game with config fetched from server
+            return {
+                ...state,
+                ...payload,
+            }
+        default:
+            throw new Error()
+    }
+}
+
+const StateProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    return <Provider value={{ state, dispatch }}>{children}</Provider>
+}
+
+export { store, StateProvider }
