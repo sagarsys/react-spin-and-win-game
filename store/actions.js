9cc72db8d0898458dc78f814b7dc6d06fd95e530
sagar <sagar_sys@live.com>
2020-10-26 10:24:46 +0400

    - add actions + update initial state & store

--- store/actions.js
+++ store/actions.js
@@ -1,2 +1,4 @@
 export const INIT_GAME = 'Init game'
-export const SET_SEGMENTS_TO_SPIN = 'Set segments to spin'
+export const SYNC_SERVER_RESPONSE = 'Sync server response'
+export const SET_GAME_STATUS = 'Set game status'
+export const SET_SPINNING_STATUS = 'Set spinning status'
--- store/store.js
+++ store/store.js
@@ -1,25 +1,39 @@
 import React, { createContext, useReducer } from 'react'
-import { INIT_GAME } from './actions'
+import {
+    INIT_GAME,
+    SET_GAME_STATUS,
+    SET_SPINNING_STATUS,
+    SYNC_SERVER_RESPONSE,
+} from './actions'
 
 const initialState = {
     initialized: false,
+    numOfSegmentsToSpin: 0,
+    isSpinning: false,
+    win: false,
+    draw: false,
+    bonus: false,
+    _id: null,
+    balance: null,
+    currentSegment: null,
 }
 const store = createContext(initialState)
 const { Provider } = store
 
 function reducer(state, action) {
-    const { type } = action
+    const { type, payload } = action
     // console.log('reducer', state, action)
     switch (type) {
         case INIT_GAME:
-            const { payload } = action
-            // init game with config fetched from server
+        case SYNC_SERVER_RESPONSE:
+        case SET_GAME_STATUS:
+        case SET_SPINNING_STATUS:
             return {
                 ...state,
                 ...payload,
             }
         default:
-            throw new Error()
+            throw new Error('Action not implemented')
     }
 }
 
